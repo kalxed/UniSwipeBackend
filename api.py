2577979a90dcc01370de9aaf9b523910e259509f
Kai Davidson <kai@davidson.to>
2024-07-22 12:02:06 -0400

    Requirements and api code updated

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
+import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from model import * # TODO import necessary functions
+#from model import * # TODO import necessary functions
 
 app = Flask(__name__)
 CORS(app)
@@ -8,15 +9,15 @@ CORS(app)
 @app.route('/compute', methods = ['POST'])
 def compute(): 
     data = request.json 
-
+    print(data)
     # accept inputs
     swipe_direction = data.get('swipe_direction')
 
     # set result
-    result = testfcn([swipe_direction])
+    result = "Calling worked correctly"
     json_result = jsonify({'result': result})
 
     return json_result
 
 if __name__ == '__main__':
-    app.run(host = "0.0.0.0", port=5000, debug = True)
+    app.run()
